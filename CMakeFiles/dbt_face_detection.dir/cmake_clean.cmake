b123f0959010992b40a5c43e707db5c92a8830af
Ludek2 <ludek.navratil@yahoo.co.uk>
2016-09-25 20:42:43 +0100

    final

---
+++ CMakeFiles/dbt_face_detection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dbt_face_detection.dir/dbt_face_detection.cpp.o"
+  "dbt_face_detection.pdb"
+  "dbt_face_detection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/dbt_face_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
